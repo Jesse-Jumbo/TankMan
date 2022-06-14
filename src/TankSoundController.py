230ea491eb93b667d2784debff48f8ad1d601ea9
Jesse <jessie97013@gmail.com>
2022-06-14 16:54:57 +0800

    refactor music volom

--- src/TankSoundController.py
+++ src/TankSoundController.py
@@ -10,12 +10,12 @@ class TankSoundController(SoundController):
     TOUCH_PATH = path.join(SOUND_DIR, "touch.wav")
 
     def play_bgm(self):
-        self.play_music(self.BGM_PATH, 0.2)
+        self.play_music(self.BGM_PATH, 0.1)
 
     def play_shoot_sound(self):
-        self.play_sound(self.SHOOT_PATH, 0.6, -1)
+        self.play_sound(self.SHOOT_PATH, 0.03, -1)
 
     def play_touch_sound(self):
-        self.play_sound(self.TOUCH_PATH, 0.6, -1)
+        self.play_sound(self.TOUCH_PATH, 0.1, -1)
 
 
