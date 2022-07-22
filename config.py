64899c48a01d5d18d385a8282f283b2113de9a84
JesseJumbo <jessie97013@gmail.com>
2022-07-22 15:41:16 +0800

    fixed path bug

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import sys
 from os import path
-from games.TankMan.src.TankMan import TankMan
+
+sys.path.append(path.dirname(__file__))
+
+from src.TankMan import TankMan
 
 GAME_SETUP = {
     "game": TankMan,
 }
 
-sys.path.append(path.dirname(__file__))
