3ca09b8be8d6f857cfe5a4b52c23db7153823ed7
JesseJumbo <jessie97013@gmail.com>
2022-08-22 11:46:56 +0800

    Del main.py
    because we always with ai to start up the game.

--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-import pygame
-from src import TankMan
+import subprocess
 
-from mlgame.view.view import PygameView
-from mlgame.gamedev.generic import quit_or_esc
 
-FPS = 30
-if __name__ == '__main__':
-    pygame.init()
-    game = TankMan.TankMan(map_no=1, frame_limit=300, sound="off")
-    scene_init_info_dict = game.get_scene_init_data()
-    game_view = PygameView(scene_init_info_dict)
-    frame_count = 0
-    while game.is_running() and not quit_or_esc():
-        clock = pygame.time.Clock()
-        clock.tick_busy_loop(FPS)
-        # TODO how set game title
-        result = game.update(game.get_keyboard_command())
-        game_progress_data = game.get_scene_progress_data()
-        game_view.draw(game_progress_data)
-        frame_count += 1
-        if result == "RESET":
-            game.reset()
-            game_view.reset()
-
-    pygame.quit()
-
+# subprocess.Popen('ls -la', shell=True)
+subprocess.Popen('-m mlgame -f 120 -i path/to/ml/ml_play_manual.py path/to/TankMan --map_no 3 --sound off --frame_limit 3000', shell=True)
